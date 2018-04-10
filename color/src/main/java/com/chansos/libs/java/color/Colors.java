f7f72e128287d534b107ba097114fc3bc41c7acb
ChangedenChan <1499450463@QQ.com>
2018-04-10 14:35:01 +0800

    Initial commit

---
+++ color/src/main/java/com/chansos/libs/java/color/Colors.java
@@ -0,0 +1,160 @@
+package com.chansos.libs.java.color;
+
+/**
+ * 颜色表
+ *
+ * @author Changeden
+ * @since 2018年4月10日
+ */
+public class Colors {
+    public static final java.awt.Color BLACK = new java.awt.Color(0x000000);
+    public static final java.awt.Color DIM_GRAY = new java.awt.Color(0x696969);
+    public static final java.awt.Color GRAY = new java.awt.Color(0x808080);
+    public static final java.awt.Color SLATE_GRAY = new java.awt.Color(0x708090);
+    public static final java.awt.Color LIGHT_SLATE_GRAY = new java.awt.Color(0x778899);
+    public static final java.awt.Color DAK_GRAY = new java.awt.Color(0xA9A9A9);
+    public static final java.awt.Color SILVER = new java.awt.Color(0xC0C0C0);
+    public static final java.awt.Color LIGHT_GRAY = new java.awt.Color(0xD3D3D3);
+    public static final java.awt.Color GAINSBORO = new java.awt.Color(0xDCDCDC);
+    public static final java.awt.Color WHITE_SMOKE = new java.awt.Color(0xF5F5F5);
+    public static final java.awt.Color GHOST_WHITE = new java.awt.Color(0xF8F8FF);
+    public static final java.awt.Color WHITE = new java.awt.Color(0xFFFFFF);
+
+
+    public static final java.awt.Color MAROON = new java.awt.Color(0x800000);
+    public static final java.awt.Color DARK_RED = new java.awt.Color(0x8B0000);
+    public static final java.awt.Color BROWN = new java.awt.Color(0xA52A2A);
+    public static final java.awt.Color FIRE_BRICK = new java.awt.Color(0xB22222);
+    public static final java.awt.Color CRIMSON = new java.awt.Color(0xDC143C);
+    public static final java.awt.Color RED = new java.awt.Color(0xFF0000);
+
+
+    public static final java.awt.Color MEDIUM_VIOLET_RED = new java.awt.Color(0xC71585);
+    public static final java.awt.Color PALE_VIOLET_RED = new java.awt.Color(0xD87093);
+    public static final java.awt.Color DEEP_PINK = new java.awt.Color(0xFF1493);
+    public static final java.awt.Color FUCHSIA = new java.awt.Color(0xFF00FF);
+    public static final java.awt.Color MAGENTA = FUCHSIA;
+    public static final java.awt.Color HOT_PINK = new java.awt.Color(0xFF69B4);
+    public static final java.awt.Color PINK = new java.awt.Color(0xFFC0CB);
+    public static final java.awt.Color LIGHT_PINK = new java.awt.Color(0xFFB6C1);
+    public static final java.awt.Color MISTY_ROSE = new java.awt.Color(0xFFE4E1);
+    public static final java.awt.Color LAVENDER_BLUSH = new java.awt.Color(0xFFF0F5);
+
+
+    public static final java.awt.Color INDIGO = new java.awt.Color(0x4B0082);
+    public static final java.awt.Color PURPLE = new java.awt.Color(0x800080);
+    public static final java.awt.Color DARK_MAGENTA = new java.awt.Color(0x8B008B);
+    public static final java.awt.Color DARK_ORCHID = new java.awt.Color(0x9932CC);
+    public static final java.awt.Color BLUE_VIOLET = new java.awt.Color(0x8A2BE2);
+    public static final java.awt.Color DARK_VIOLET = new java.awt.Color(0x9400D3);
+    public static final java.awt.Color SLATE_BLUE = new java.awt.Color(0x6A5ACD);
+    public static final java.awt.Color MEDIUM_PURPLE = new java.awt.Color(0x9370DB);
+    public static final java.awt.Color MEDIUM_SLATE_BLUE = new java.awt.Color(0x7B68EE);
+    public static final java.awt.Color MEDIUM_ORCHID = new java.awt.Color(0xBA55D3);
+    public static final java.awt.Color VIOLET = new java.awt.Color(0xEE82EE);
+    public static final java.awt.Color PLUM = new java.awt.Color(0xDDA0DD);
+    public static final java.awt.Color THISTLE = new java.awt.Color(0xD8BFD8);
+    public static final java.awt.Color LAVENDER = new java.awt.Color(0xE6E6FA);
+
+
+    public static final java.awt.Color SADDLE_BROWN = new java.awt.Color(0x8B4513);
+    public static final java.awt.Color SIENNA = new java.awt.Color(0xA0522D);
+    public static final java.awt.Color CHOCOLATE = new java.awt.Color(0xD2691E);
+    public static final java.awt.Color INDIAN_RED = new java.awt.Color(0xCD5C5C);
+    public static final java.awt.Color ROSY_BROWN = new java.awt.Color(0xBC8F8F);
+    public static final java.awt.Color LIGHT_COROL = new java.awt.Color(0xF08080);
+    public static final java.awt.Color SALMON = new java.awt.Color(0xFA8072);
+    public static final java.awt.Color LIGHT_SALMON = new java.awt.Color(0xFFA07A);
+    public static final java.awt.Color ORANGE_RED = new java.awt.Color(0xFF4500);
+    public static final java.awt.Color TOMATO = new java.awt.Color(0xFF6347);
+    public static final java.awt.Color CORAL = new java.awt.Color(0xFF7F50);
+    public static final java.awt.Color DARK_ORANGE = new java.awt.Color(0xFF8C00);
+    public static final java.awt.Color SANDY_BROWN = new java.awt.Color(0xF4A460);
+    public static final java.awt.Color PERU = new java.awt.Color(0xCD853F);
+    public static final java.awt.Color TAN = new java.awt.Color(0xD2B48C);
+    public static final java.awt.Color BURLY_WOOD = new java.awt.Color(0xDEB887);
+    public static final java.awt.Color WHEAT = new java.awt.Color(0xF5DEB3);
+    public static final java.awt.Color MOCCASIN = new java.awt.Color(0xFFE4B5);
+    public static final java.awt.Color NAVAJO_WHITE = new java.awt.Color(0xFFDEAD);
+    public static final java.awt.Color PEACH_PUFF = new java.awt.Color(0xFFDAB9);
+    public static final java.awt.Color BISQUE = new java.awt.Color(0xFFE4C4);
+    public static final java.awt.Color ANTUQUE_WHITE = new java.awt.Color(0xFAEBD7);
+    public static final java.awt.Color PAPAYAWHIP = new java.awt.Color(0xFFEFD5);
+    public static final java.awt.Color CORNSILK = new java.awt.Color(0xFFF8DC);
+    public static final java.awt.Color OLDLACE = new java.awt.Color(0xFDF5E6);
+    public static final java.awt.Color LINEN = new java.awt.Color(0xFAF0E6);
+    public static final java.awt.Color SEASHELL = new java.awt.Color(0xFFF5EE);
+    public static final java.awt.Color SNOW = new java.awt.Color(0xFFFAFA);
+    public static final java.awt.Color FLORAL_WHITE = new java.awt.Color(0xFFFAF0);
+    public static final java.awt.Color IVORY = new java.awt.Color(0xFFFFF0);
+    public static final java.awt.Color MINT_CREAM = new java.awt.Color(0xF5FFFA);
+
+
+    public static final java.awt.Color DARK_GOLEDNROD = new java.awt.Color(0xB8860B);
+    public static final java.awt.Color GOLDENROD = new java.awt.Color(0xDAA520);
+    public static final java.awt.Color GLOD = new java.awt.Color(0xFFD700);
+    public static final java.awt.Color YELLOW = new java.awt.Color(0xFFFF00);
+    public static final java.awt.Color DARK_KHAKI = new java.awt.Color(0xBDB76B);
+    public static final java.awt.Color KHAKI = new java.awt.Color(0xF0E68C);
+    public static final java.awt.Color PALE_GOLDENROD = new java.awt.Color(0xEEE8AA);
+    public static final java.awt.Color BEIGE = new java.awt.Color(0xF5F5DC);
+    public static final java.awt.Color LEMON_CHIFFON = new java.awt.Color(0xFFFACD);
+    public static final java.awt.Color LIGHT_GOLDENROD_YELLOW = new java.awt.Color(0xFAFAD2);
+    public static final java.awt.Color LIGHT_YELLOW = new java.awt.Color(0xFFFFE0);
+
+
+    public static final java.awt.Color DARK_SLATE_GRAY = new java.awt.Color(0x2F4F4F);
+    public static final java.awt.Color DARK_OLIVE_GREEN = new java.awt.Color(0x556B2F);
+    public static final java.awt.Color OLIVE = new java.awt.Color(0x808000);
+    public static final java.awt.Color DARK_GREEN = new java.awt.Color(0x006400);
+    public static final java.awt.Color FOREST_GEEN = new java.awt.Color(0x228B22);
+    public static final java.awt.Color SEA_GREEN = new java.awt.Color(0x2E8B57);
+    public static final java.awt.Color GREEN = new java.awt.Color(0x00FF00);
+    public static final java.awt.Color TEAL = new java.awt.Color(0x008080);
+    public static final java.awt.Color LIGHT_SEA_GREEN = new java.awt.Color(0x20B2AA);
+    public static final java.awt.Color MADIUM_AQUAMARINE = new java.awt.Color(0x66CDAA);
+    public static final java.awt.Color MEDIUM_SEA_GREEN = new java.awt.Color(0x3CB371);
+    public static final java.awt.Color DARK_SEA_GREEN = new java.awt.Color(0x8FBC8F);
+    public static final java.awt.Color YELLOW_GREEN = new java.awt.Color(0x9ACD32);
+    public static final java.awt.Color LIME_GREEN = new java.awt.Color(0x32CD32);
+    public static final java.awt.Color LIME = GREEN;
+    public static final java.awt.Color CHART_REUSE = new java.awt.Color(0x7FFF00);
+    public static final java.awt.Color LAWN_GREEN = new java.awt.Color(0x7CFC00);
+    public static final java.awt.Color GREEN_YELLOW = new java.awt.Color(0xADFF2F);
+    public static final java.awt.Color MEDIUM_SPRING_GREEN = new java.awt.Color(0x00FA9A);
+    public static final java.awt.Color SPRING_GREEN = new java.awt.Color(0x00FF7F);
+    public static final java.awt.Color LIGHT_GREEN = new java.awt.Color(0x90EE90);
+    public static final java.awt.Color PALE_GREEN = new java.awt.Color(0x98F898);
+    public static final java.awt.Color AQUAMARINE = new java.awt.Color(0x7FFFD4);
+    public static final java.awt.Color HONEYDEW = new java.awt.Color(0xF0FFF0);
+
+
+    public static final java.awt.Color MIDNIGHT_BLUE = new java.awt.Color(0x191970);
+    public static final java.awt.Color NAVY = new java.awt.Color(0x000080);
+    public static final java.awt.Color DARK_BLUE = new java.awt.Color(0x00008B);
+    public static final java.awt.Color DARK_SLATE_BLUE = new java.awt.Color(0x483D8B);
+    public static final java.awt.Color MEDIUM_BLUE = new java.awt.Color(0x0000CD);
+    public static final java.awt.Color ROYAL_BLUE = new java.awt.Color(0x4169E1);
+    public static final java.awt.Color DODGER_blue = new java.awt.Color(0x1E90FF);
+    public static final java.awt.Color CORNFLOWER_BLUE = new java.awt.Color(0x6495ED);
+    public static final java.awt.Color DEEP_SKY_BLUE = new java.awt.Color(0x00BFFF);
+    public static final java.awt.Color LIGHT_SKY_BLUE = new java.awt.Color(0x87CEFA);
+    public static final java.awt.Color LIGHT_STEEL_BLUE = new java.awt.Color(0xB0C4DE);
+    public static final java.awt.Color LIGHT_BLUE = new java.awt.Color(0xADD8E6);
+    public static final java.awt.Color STEEL_BLUE = new java.awt.Color(0x4682B4);
+    public static final java.awt.Color DARK_CYAN = new java.awt.Color(0x008B8B);
+    public static final java.awt.Color CADET_BLUE = new java.awt.Color(0x5F9EA0);
+    public static final java.awt.Color DARK_TURQUOISE = new java.awt.Color(0x00CED1);
+    public static final java.awt.Color MEDIUM_TURQUOISE = new java.awt.Color(0x48D1CC);
+    public static final java.awt.Color TURQUOISE = new java.awt.Color(0x40E0D0);
+    public static final java.awt.Color SKY_BLUE = new java.awt.Color(0x87CECB);
+    public static final java.awt.Color POWDER_BLUE = new java.awt.Color(0xB0E0E6);
+    public static final java.awt.Color PALE_TURQUOISE = new java.awt.Color(0xAFEEEE);
+    public static final java.awt.Color LIGHT_CYAN = new java.awt.Color(0xE0FFFF);
+    public static final java.awt.Color AZURE = new java.awt.Color(0xF0FFFF);
+    public static final java.awt.Color ALICE_BLUE = new java.awt.Color(0xF0F8FF);
+    public static final java.awt.Color AQUA = new java.awt.Color(0x00FFFF);
+    public static final java.awt.Color CYAN = AQUA;
+    public static final java.awt.Color BLUE = new java.awt.Color(0x0000FF);
+
+}
